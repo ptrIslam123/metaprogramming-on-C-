533e4d73eb13f7aaea3b8835e668ff9ac75a93b3
ptrIslam123 <asker.novikov@bk.ru>
2021-07-05 21:41:18 +0300

    create struct tuple_size

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <assert.h>
 #include <memory>
 #include <type_traits>
+#include <tuple>
+
 
 //#include "template_list/template_list.h"
 #include "tuple/tuple.h"
@@ -9,16 +11,41 @@
 
 
 
-
-
-
 int main()
 {   
-
-    
-    using tuple_t = meta::tuple_helper<int, char>;
-
-
+   
     return 0;
 }
 
+/*
+
+
+template<std::size_t pos>
+struct pos_t
+{};
+
+
+template<class T, std::size_t indx>
+void print(const T& t, pos_t<indx> pos)
+{
+    std::cout << std::get<std::tuple_size<T>::value - indx>(t) << ", ";
+    print(t, pos_t<indx - 1>());
+}
+
+
+template<class T>
+void print(const T& t, pos_t<1> )
+{
+    std::cout << std::get<std::tuple_size<T>::value - 1>(t);
+}
+
+
+template<class T, std::size_t size>
+void print_tuple(const T& t)
+{
+    std::cout << "{ ";
+        print(t, pos_t<size>());
+    std::cout << " }\n";
+}
+
+*/
--- tuple/tuple.h
+++ tuple/tuple.h
@@ -9,14 +9,24 @@
 namespace meta
 {
 
+    template<class ... Args>
+    struct tuple;
+
+
+    template<class T>
+    struct tuple_size;
+
+
     template<std::size_t _index>
     struct element_index
     {
         static constexpr std::size_t index = _index;
     };
 
+
     template<class ... Args>
     struct tuple_helper;
+
 
     template<>
     struct tuple_helper<>
@@ -24,28 +34,67 @@ namespace meta
 
 
 
+    template<class ... Args>
+    struct tuple
+    {
+        typedef tuple_helper<Args ...>              helper_type;
+
+        tuple(const Args& ... args);
+
+        const helper_type&                          get_helper() const; 
+        
+
+    private:
+        helper_type                                  _helper;
+    };
+
+
+
+    template<class ... Args>
+    tuple<Args ...>::tuple(const Args& ... args):
+        _helper(helper_type(args ...))
+    {}
+
+
+    template<class ... Args>
+    const typename tuple<Args ...>::helper_type& 
+    tuple<Args ...>::get_helper() const
+    {
+        return _helper;
+    }
+
+
+
+
     template<class T>
-    void print_tuple(const T& tuple);
+    struct tuple_size
+    {
+        typedef typename T::helper_type             tuple_type;
+        typedef typename tuple_type::size_type      size_value_type;
+    };
+
+
+
 
 
     template<class T, class ... Args>
     struct tuple_helper<T, Args ...>
     {
-        typedef T                                   value_type;
-        typedef tuple_helper<Args ...>              tail_type;
-        typedef element_index<sizeof ... (Args)>    pos_type;
+        typedef T                                       value_type;
+        typedef tuple_helper<Args ...>                  tail_type;
+        typedef element_index<sizeof ... (Args) + 1>    size_type;
 
         tuple_helper(const value_type& value, const Args& ... args);
 
         const value_type&                           get_value() const;
         const tail_type&                            get_tail() const;
-        constexpr std::size_t                       get_pos() const;
+        constexpr std::size_t                       get_size() const;
         
 
     private:
         value_type                                  _value;
         tail_type                                   _tail;
-        pos_type                                    _pos;
+        size_type                                   _pos;
     };
 
 
@@ -73,48 +122,11 @@ namespace meta
 
     template<class T, class ... Args>
     constexpr std::size_t
-    tuple_helper<T, Args ...>::get_pos() const
+    tuple_helper<T, Args ...>::get_size() const
     {
         return _pos.index;
     } 
 
-
-    
-    template<class T>
-    struct print_tuple_element;
-
-    template<>
-    struct print_tuple_element<element_index<0>>;
-
-   
-    template<class T>
-    void print_tuple(const T& tuple)
-    {
-        print_tuple_element<T> p;
-        
-        std::cout << "{";
-            p(tuple);   
-        std::cout << "}\n";
-    }
-
-
-    template<class T>
-    struct print_tuple_element
-    {
-        void operator () (const T& tuple)
-        {
-            //print_tuple_element<decltype(tuple.get_tail())> p;
-
-            std::cout << tuple.get_value();
-            //p(tuple.get_tail());
-        }
-    };
-
-    template<>
-    struct print_tuple_element<element_index<0>>
-    {};
-
-    
 
 
 } // namespace meta::end;
